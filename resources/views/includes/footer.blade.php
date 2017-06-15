1604d7d7721eb2e6daa192678de96df2094c0b77
Kania Amalia <kaniaamalia21@gmail.com>
2017-06-15 10:48:25 +0700

    aa

--- resources/views/includes/footer.blade.php
+++ resources/views/includes/footer.blade.php
@@ -5,15 +5,15 @@
 }
 </style>
  <!-- jQuery -->
-    <script src="{{url('vendor/jquery/jquery.min.js')}}"></script>
+    <script src="{{url('../vendor/jquery/jquery.min.js')}}"></script>
 
     <!-- Bootstrap Core JavaScript -->
-    <script src="{{url('vendor/bootstrap/js/bootstrap.min.js')}}"></script>
+    <script src="{{url('../vendor/bootstrap/js/bootstrap.min.js')}}"></script>
 
     <!-- Plugin JavaScript -->
     <script src="https://cdnjs.cloudflare.com/ajax/libs/jquery-easing/1.3/jquery.easing.min.js"></script>
-    <script src="{{url('vendor/scrollreveal/scrollreveal.min.js')}}"></script>
-    <script src="{{url('vendor/magnific-popup/jquery.magnific-popup.min.js')}}"></script>
+    <script src="{{url('../vendor/scrollreveal/scrollreveal.min.js')}}"></script>
+    <script src="{{url('../vendor/magnific-popup/jquery.magnific-popup.min.js')}}"></script>
 
     <!-- Theme JavaScript -->
     <script src="{{url('js/creative.min.js')}}"></script>
--- resources/views/includes/head.blade.php
+++ resources/views/includes/head.blade.php
@@ -7,15 +7,15 @@
     <title>IMS - Internship Management System   </title>
 
     <!-- Bootstrap Core CSS -->
-   <link href="{{url('vendor/bootstrap/css/bootstrap.min.css')}}" rel="stylesheet">
+   <link href="{{url('../vendor/bootstrap/css/bootstrap.min.css')}}" rel="stylesheet">
 
     <!-- Custom Fonts -->
-    <link href="{{url('vendor/font-awesome/css/font-awesome.min.css')}}" rel="stylesheet" type="text/css">
+    <link href="{{url('../vendor/font-awesome/css/font-awesome.min.css')}}" rel="stylesheet" type="text/css">
     <link href='https://fonts.googleapis.com/css?family=Open+Sans:300italic,400italic,600italic,700italic,800italic,400,300,600,700,800' rel='stylesheet' type='text/css'>
     <link href='https://fonts.googleapis.com/css?family=Merriweather:400,300,300italic,400italic,700,700italic,900,900italic' rel='stylesheet' type='text/css'>
 
     <!-- Plugin CSS -->
-    <link href="{{url('vendor/magnific-popup/magnific-popup.css')}}" rel="stylesheet">
+    <link href="{{url('../vendor/magnific-popup/magnific-popup.css')}}" rel="stylesheet">
 
     <!-- Theme CSS -->
     <link href="{{url('css/creative.css')}}" rel="stylesheet">
